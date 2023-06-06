2e1843e7cee8e446431ab88268f70df5b31114b1
Yj <gbionoak01@gmail.com>
2023-06-06 22:39:35 +0900

    renderless 컴포넌트

--- vue-components/design/renderless/src/App.vue
+++ vue-components/design/renderless/src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <fetch-data url="https://jsonplaceholder.typicode.com/users/1">
-      <!-- ... -->
+      <div slot-scope="{response}, {loading}">
+        <div v-if="!loading">
+          {{ response }}
+        </div>
+        <div v-if="loading">
+          Loading...
+        </div>
+      </div>
     </fetch-data>
   </div>
 </template>
